017c51f63a268b7de69d27025c7e24f01edeb4a0
JuMn88 <moineau.julien@hotmail.fr>
2021-05-11 17:44:55 +0200

    correcting database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `user` (
-    `id` INT  NOT NULL ,
-    `username` VARCHAR(50)  NOT NULL ,
-    `profile_picture` VARCHAR(255)  NOT NULL ,
-    `profile_certified` BOOL  NOT NULL ,
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50),
+    profile_picture VARCHAR(255),
+    profile_certified BOOL,
     PRIMARY KEY (
-        `id`
+        id
     )
 );
 
@@ -26,33 +26,33 @@ INSERT INTO user (username, profile_picture, profile_certified) VALUES
 ('O-Mars-y', 'o-mars-y.jpeg', 0),
 ('Matt Damon', 'matt_damon.jpg', 1);
 
-CREATE TABLE `message` (
-    `id` INT  NOT NULL ,
-    `content` TEXT  NOT NULL ,
-    `post_date` DATE  NOT NULL ,
-    `user_id` INT  NOT NULL ,
-    `photo_id` INT  NOT NULL ,
+CREATE TABLE message (
+    id INT NOT NULL AUTO_INCREMENT,
+    content TEXT,
+    post_date DATE,
+    user_id INT,
+    photo_id INT,
     PRIMARY KEY (
-        `id`
+        id
     )
 );
 
-CREATE TABLE `photo` (
-    `id` INT  NOT NULL ,
-    `name` VARCHAR(255)  NOT NULL ,
-    `url` VARCHAR(255)  NOT NULL ,
-    `description` TEXT  NOT NULL ,
-    `user_id` INT  NOT NULL ,
+CREATE TABLE photo (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    url VARCHAR(255),
+    description TEXT,
+    user_id INT,
     PRIMARY KEY (
-        `id`
+        id
     )
 );
 
-ALTER TABLE `message` ADD CONSTRAINT `fk_message_user_id` FOREIGN KEY(`user_id`)
-REFERENCES `user` (`id`);
+ALTER TABLE message ADD CONSTRAINT fk_message_user_id FOREIGN KEY(user_id)
+REFERENCES user (id);
 
-ALTER TABLE `message` ADD CONSTRAINT `fk_message_photo_id` FOREIGN KEY(`photo_id`)
-REFERENCES `photo` (`id`);
+ALTER TABLE message ADD CONSTRAINT fk_message_photo_id FOREIGN KEY(photo_id)
+REFERENCES photo (id);
 
-ALTER TABLE `photo` ADD CONSTRAINT `fk_photo_user_id` FOREIGN KEY(`user_id`)
-REFERENCES `user` (`id`);
+ALTER TABLE photo ADD CONSTRAINT fk_photo_user_id FOREIGN KEY(user_id)
+REFERENCES user (id);
